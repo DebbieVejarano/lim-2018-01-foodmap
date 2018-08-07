930a41ffd8bac713e66f54332b01902bf2277dc9
alexandra.vejarano@gmail.com <alexandra.vejarano@gmail.com>
2018-08-07 17:59:22 -0500

    closes #2

--- src/js/app.js
+++
@@ -1,8 +0,0 @@
-/* variables */
-const splashView = document.querySelector('.flex-container')
-
-setTimeout(()=>{
-/* splashView.style.display = 'none' */
-/* alert('hola, universo') */
-}, 2000)
-
---
+++ src/js/main.js
@@ -0,0 +1,12 @@
+/* variables */
+const splash = document.querySelector('.flex-container')
+const principal = document.getElementById('principal')
+
+principal.style.display = 'none'
+
+setTimeout(() => {
+    splash.style.display = 'none'
+    principal.style.display = 'block'
+    document.body.style.background = '#ffffff'
+     document.body.style.display = 'block'
+    }, 2000);
